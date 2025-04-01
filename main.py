bcab475c1353a5692ad5cec744b4e2c0d0bc8ad1
LAV419 <73648857+LAV419@users.noreply.github.com>
2025-04-01 16:07:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# (C) Copyright Peter Hinch 2017-2019.
-# Released under the MIT licence.
-
-# This demo publishes to topic "result" and also subscribes to that topic.
-# This demonstrates bidirectional TLS communication.
-# You can also run the following on a PC to verify:
-# mosquitto_sub -h test.mosquitto.org -t result
-# To get mosquitto_sub to use a secure connection use this, offered by @gmrza:
-# mosquitto_sub -h <my local mosquitto server> -t result -u <username> -P <password> -p 8883
-
-# Public brokers https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
-
-# red LED: ON == WiFi fail
-# green LED heartbeat: demonstrates scheduler is running.
-
 from mqtt_as import MQTTClient
 from mqtt_local import config
 import uasyncio as asyncio
