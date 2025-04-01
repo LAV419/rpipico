fcf0d277faa5ffb697f3b8e3cff16f716aaa7ee7
LAV419 <73648857+LAV419@users.noreply.github.com>
2025-04-01 16:02:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,42 +16,123 @@
 from mqtt_as import MQTTClient
 from mqtt_local import config
 import uasyncio as asyncio
+import dht, machine, ujson
+
+d = dht.DHT22(machine.Pin(15))
+relay = machine.Pin(2, machine.Pin.OUT)
+led_board = machine.Pin("LED", machine.Pin.OUT) 
+
+event_mqtt = asyncio.Event()
+mqtt_messages = []
+
+id = "".join("{:02X}".format(b) for b in machine.unique_id())
+print(id)
+
+def load_params():
+    try:
+        with open("params.json", "r") as f:
+            params = ujson.load(f)
+    except (OSError, ValueError):
+        params = {"setpoint": 25, "periodo": 10, "modo": "auto", "rele": 0}
+        save_params(params)
+    return params
+
+def save_params(params):
+    with open("params.json", "w") as f:
+        ujson.dump(params, f)
+
+params = load_params()
+relay.value(params["rele"])
 
 SERVER = config['server']
 
-def sub_cb(topic, msg, retained):
-    c, r = [int(x) for x in msg.decode().split(' ')]
-    print('Topic = {} Count = {} Retransmissions = {} Retained = {}'.format(topic.decode(), c, r, retained))
+def mqtt_handler(topic, msg, retained):
+    global mqtt_messages
+    mqtt_messages.append((topic.decode(), msg.decode()))
+    event_mqtt.set()
+
+async def process_mqtt_messages():
+    global mqtt_messages, params
+    while True:
+        await event_mqtt.wait()
+        while mqtt_messages:
+            topic, msg = mqtt_messages.pop(0)
+            print(f"Procesando: {topic} -> {msg}")
+
+            if topic.endswith("/setpoint"):
+                params["setpoint"] = int(msg)
+            elif topic.endswith("/periodo"):
+                params["periodo"] = int(msg)
+            elif topic.endswith("/modo"):
+                params["modo"] = msg
+            elif topic.endswith("/rele"):
+                asyncio.create_task(rele(msg))
+            elif topic.endswith("/destello"):
+                asyncio.create_task(destello())
+            
+            save_params(params)
+        event_mqtt.clear()
+
+async def destello():
+    for _ in range(5):
+        led_board.value(1) 
+        await asyncio.sleep(0.5)
+        led_board.value(0) 
+        await asyncio.sleep(0.5)
+
+async def rele(msg):
+    if msg == "rele":
+        params["rele"] = 1 - params["rele"]
+        relay.value(params["rele"])
+        await asyncio.sleep(0.5)
+
 
 async def wifi_han(state):
-    print('Wifi is ', 'up' if state else 'down')
+    print("WiFi", "conectado" if state else "desconectado")
     await asyncio.sleep(1)
 
-# If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
 async def conn_han(client):
-    await client.subscribe('result', 1)
+    topics = ["setpoint", "periodo", "modo", "rele", "destello"]
+    for t in topics:
+        await client.subscribe(f"{id}/{t}", 1)
+        await asyncio.sleep(0)
 
 async def main(client):
     await client.connect()
-    n = 0
-    await asyncio.sleep(2)  # Give broker time
+    asyncio.create_task(process_mqtt_messages())
+    await asyncio.sleep(2)
+
     while True:
-        print('publish', n)
-        # If WiFi is down the following will pause for the duration.
-        await client.publish('result', '{} {}'.format(n, client.REPUB_COUNT), qos = 1)
-        n += 1
-        await asyncio.sleep(10)  # Broker is slow
+        d.measure()
+        temp = d.temperature()
+        hum = d.humidity()
 
-# Define configuration
-config['subs_cb'] = sub_cb
-config['server'] = SERVER
-config['connect_coro'] = conn_han
-config['wifi_coro'] = wifi_han
-config['ssl'] = True
+        if params["modo"] == "auto":
+            relay.value(1 if temp < params["setpoint"] else 0)
+        
+        payload = ujson.dumps({
+            "temperatura": temp,
+            "humedad": hum,
+            "setpoint": params["setpoint"],
+            "periodo": params["periodo"],
+            "modo": params["modo"],
+            "rele": relay.value(),
+        })
+        await client.publish(f"{id}", payload, qos=1)
 
-# Set up client
-MQTTClient.DEBUG = True  # Optional
+        await asyncio.sleep(params["periodo"])
+
+config.update({
+    "subs_cb": mqtt_handler,
+    "server": SERVER,
+    "connect_coro": conn_han,
+    "wifi_coro": wifi_han,
+    "ssl": True,
+})
+
+MQTTClient.DEBUG = True
 client = MQTTClient(config)
+
 try:
     asyncio.run(main(client))
 finally:
